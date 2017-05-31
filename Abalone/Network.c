f314d69c46ee6aa6a1e5547ecd61b6164f8961de
Valentin DEBENATH <valentin.debenath@gmail.com>
2017-05-31 21:08:53 +0200

    Finalisation réseau de neurones

--- Abalone/Network.c
+++ Abalone/Network.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <math.h>
 #include "Matrix2D.h"
 #include "Network.h"
 
@@ -11,28 +12,14 @@ void myRand(double *x)
 	*x = (double)(rand() - RAND_MAX / 2) / (double)(RAND_MAX / 2);
 }
 
-void myRand2D(Matrix2D *x)
-{
-	applyMatrixElement(myRand, x->mat);
-}
-
 void sigmoide(double *x)
 {
 	*x = 1 / (1 + exp(-(*x)*Lambda));
-}
-void sigmoide2D(Matrix2D *x)
-{
-	applyMatrixElement(sigmoide, x->mat);
 }
 
 void der_sigmoide(double *x)
 {
 	*x = Lambda * exp(-(*x)*Lambda) / pow((1 + exp(-(*x)*Lambda)), 2);
-}
-
-void der_sigmoide2D(Matrix2D *x)
-{
-	applyMatrixElement(der_sigmoide, x->mat);
 }
 
 Network* newEmptyNetwork()
@@ -48,6 +35,7 @@ Network* newEmptyNetwork()
 
 Network* newNetwork(Matrix* layerSize) 
 {
+	unsigned int i;
 	Network *n = newEmptyNetwork();
 	n->size = lengthMatrix(layerSize);
 
@@ -56,32 +44,37 @@ Network* newNetwork(Matrix* layerSize)
 	n->weights = malloc(sizeof(Matrix2D*)*(n->size - 1));
 	n->bias = malloc(sizeof(Matrix2D*)*(n->size - 1));
 
-	for (unsigned int i = 0; i < n->size; i++)
-		n->neurons[i] = newMatrix2D(getMatrixElement(layerSize,i), 1);
-	for (unsigned int i = 0; i < n->size-1; i++)
-		n->activated[i] = newMatrix2D(getMatrixElement(layerSize, i+1), 1);
-	for (unsigned int i = 0; i < n->size - 1; i++)
-		n->bias[i] = newMatrix2D(getMatrixElement(layerSize, i + 1), 1);
-	for (unsigned int i = 0; i < n->size - 1; i++)
+	for (i = 0; i < n->size; i++)
+		n->neurons[i] = newMatrix2D((unsigned int)getMatrixElement(layerSize,i), 1);
+	for (i = 0; i < n->size-1; i++)
+		n->activated[i] = newMatrix2D((unsigned int)getMatrixElement(layerSize, i+1), 1);
+	for (i = 0; i < n->size - 1; i++)
+		n->bias[i] = newMatrix2D((unsigned int)getMatrixElement(layerSize, i + 1), 1);
+	for (i = 0; i < n->size - 1; i++)
 		n->weights[i] = newMatrix2D(n->bias[i]->n, n->neurons[i]->n);
 
-	applyMatrixElement(myRand2D, n->weights);
-	applyMatrixElement(myRand2D, n->bias);
+	for (i = 0; i < n->size - 1; i++)
+		applyMatrixElement(myRand, n->weights[i]->mat);
+	for (i = 0; i < n->size - 1; i++)
+		applyMatrixElement(myRand, n->bias[i]->mat);
+
+	return n;
 }
 
 Matrix2D* trainNetwork(Network* net, Matrix2D* input, Matrix2D* output)
 {
 	Matrix2D *activated, *error;
+	unsigned int i;
 
 	copyMatrix2D(net->neurons[0], input);
 	dotInMatrix2D(net->activated[0], net->weights[0], net->neurons[0]);
 	sumInMatrix2D(net->activated[0], net->bias[0]);
 
-	for (unsigned int i = 1; i < net->size - 1; i++)
+	for (i = 1; i < net->size - 1; i++)
 	{
 		copyMatrix2D(net->neurons[i], net->activated[i - 1]);
 
-		sigmoide2D(net->neurons[i]);
+		applyMatrixElement(sigmoide, net->neurons[i]->mat);
 
 		dotInMatrix2D(net->activated[i], net->weights[i], net->neurons[i]);
 
@@ -90,7 +83,7 @@ Matrix2D* trainNetwork(Network* net, Matrix2D* input, Matrix2D* output)
 
 	activated = net->activated[net->size - 2];
 
-	applyMatrixElement(der_sigmoide2D, activated);
+	applyMatrixElement(der_sigmoide, activated->mat);
 
 	error = minusMatrix2D(net->neurons[net->size - 1], output);
 
@@ -103,7 +96,8 @@ void corrErrorNetwork(Network *net, Matrix2D* error)
 {
 	Matrix2D** delta = malloc(sizeof(Matrix2D*)*(net->size-1));
 	Matrix2D ** activated = malloc(sizeof(Matrix2D*)*(net->size - 2));
-	int i = 0;
+	unsigned int i = 0;
+	int j = 0;
 
 	for (i = 0; i < net->size - 1; i++)
 	{
@@ -112,21 +106,25 @@ void corrErrorNetwork(Network *net, Matrix2D* error)
 		applyMatrixElement(sigmoide, activated[i]->mat);
 	}
 
-	delta[net->size-2] = newMatrix2DCopy(error);
+	delta[net->size - 2] = newEmptyMatrix2D();
+	copyMatrix2D(error, delta[net->size - 2]);
 	
-	for (int i = (int)net->size - 3; i >= 0; i--)
+	for (j = (int)net->size - 3; j >= 0; j--)
 	{
-		Matrix2D *t = transp(net->weights[i + 1]);
-		dotInMatrix2D(t, t, delta[i + 1]);
-		hadamardDotInMatrix2D(t, activated[i]);
-		copyMatrix2D(t, delta[i]);
+		Matrix2D *t = transp(net->weights[j + 1]);
+		dotInMatrix2D(t, t, delta[j + 1]);
+		hadamardDotInMatrix2D(t, activated[j]);
+		copyMatrix2D(t, delta[j]);
 		deleteMatrix2D(t);
 	}
 
-	for (unsigned int i = 0; i < net->size-1; i++)
+	for (i = 0; i < net->size-1; i++)
 	{
-		*net->weights[i] += -Lambda * (*delta[i]) * net->neurons[i]->transp();
-		*net->bias[i] += -Lambda * (*delta[i]);
+		Matrix2D *t = transp(net->neurons[i]);
+		dotInMatrix2D(delta[i], delta[i], t);
+		dotScalarMatrix2D(-Lambda, delta[i]);
+		sumInMatrix2D(net->weights[i], delta[i]);
+		sumInMatrix2D(net->bias[i], delta[i]);
 	}
 
 	for (i = 0; i < net->size - 1; i++)
@@ -139,55 +137,67 @@ void corrErrorNetwork(Network *net, Matrix2D* error)
 	free(activated);
 }
 
-double trainNetworkManyTime(Network *net, Matrix2D** inputs, Matrix2D** outputs)
+double trainNetworkManyTime(Network *net, Matrix2D** inputs, Matrix2D** outputs, unsigned int inputLength)
 {
-	Matrix2D* error((unsigned int)outputs.first()->length(), 1);
+	Matrix2D* error = newMatrix2D(lengthMatrix(outputs[0]->mat), 1);
+	double mean = 0;
+	unsigned int i;
 
-	for (unsigned int i = 0; i < inputs.length(); i++)
+	for (i = 0; i < inputLength; i++)
 	{
-		unsigned int index(rand() % inputs.length());
-		Matrix2D<double> er = train(inputs[index], outputs[index]);
-		error += er;
-		corrError(er);
+		unsigned int index = rand() % inputLength;
+		
+		Matrix2D* er = trainNetwork(net, inputs[index], outputs[index]);
+		sumInMatrix2D(error, er);
+		corrErrorNetwork(net, er);
+		deleteMatrix2D(er);
 	}
 
-	error /= (double)inputs.length();
+	divisionScalarMatrix2D(inputLength, error);
 
-	double mean(0);
-
-	for (unsigned int i = 0; i < error.length(); i++)
-		mean += error[i];
-
-	mean /= (double)error.length();
+	for (i = 0; i < lengthMatrix(error->mat); i++)
+		mean += getMatrixElement(error->mat, i);
+	
+	mean /= lengthMatrix(error->mat);
 
 	return mean;
 }
 
-Matrix2D* processNetwork(Matrix2D *input)
+Matrix2D* processNetwork(Network *net, Matrix2D *input)
 {
-	m_neurons[0] = input;
-	*m_activated[0] = (*m_weights[0]) * (*m_neurons[0]) + (*m_bias[0]);
+	Matrix2D *p = dotMatrix2D(net->weights[0], net->neurons[0]);
+	unsigned int i;
 
-	for (unsigned int i = 1; i < m_activated.length(); i++)
+	copyMatrix2D(input, net->neurons[0]);
+
+	copyMatrix2D(p, net->activated[0]);
+	sumInMatrix2D(net->activated[0],net->bias[0]);
+
+	deleteMatrix2D(p);
+	
+	for (i = 1; i < net->size-1; i++)
 	{
-		m_neurons[i] = m_activated[i - 1];
+		net->neurons[i] = net->activated[i - 1];
 
-		sigmoide2D(m_neurons[i]);
-
-		*m_activated[i] = (*m_weights[i]) * (*m_neurons[i]) + (*m_bias[i]);
+		applyMatrixElement(sigmoide, net->neurons[i]->mat);
+		
+		p = dotMatrix2D(net->weights[i], net->neurons[i]);
+		copyMatrix2D(p, net->activated[i]);
+		sumInMatrix2D(net->activated[i], net->bias[i]);
+		deleteMatrix2D(p);
 	}
 
-	return *m_neurons.last();
+	return net->neurons[net->size-1];
 }
 
-void deleteNetwork()
+void deleteNetwork(Network *net)
 {
-	for (unsigned int i = 0; i < m_neurons.length(); i++)
-		delete m_neurons[i];
-	for (unsigned int i = 0; i < m_activated.length(); i++)
-		delete m_activated[i];
-	for (unsigned int i = 0; i < m_bias.length(); i++)
-		delete m_bias[i];
-	for (unsigned int i = 0; i < m_weights.length(); i++)
-		delete m_weights[i];
+	for (unsigned int i = 0; i < net->size; i++)
+		deleteMatrix2D(net->neurons[i]);
+	for (unsigned int i = 0; i < net->size; i++)
+		deleteMatrix2D(net->activated[i]);
+	for (unsigned int i = 0; i < net->size; i++)
+		deleteMatrix2D(net->bias[i]);
+	for (unsigned int i = 0; i < net->size; i++)
+		deleteMatrix2D(net->weights[i]);
 }
